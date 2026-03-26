c560e4bc6b149303a0ca54b10b122fa5c41a9a77
raphaelbtgm <raphael.btgm@gmail.com>
2026-03-26 11:34:03 +0100

    modifications

--- archi_mvc_appli_sportive/main.py
+++ archi_mvc_appli_sportive/main.py
@@ -1,5 +1,5 @@
-from controller.app_controller import AppController
-from view.console_view import show_motivation
+from controllers.app_controller import AppController
+from views.console_view import show_motivation
 
 if __name__ == "__main__":
     show_motivation()
